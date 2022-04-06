80a18778b8a5bfa403bb85e28304e0cbce3b4d1c
이환주 <hwanju@ihwanjuui-MacBook-Pro.local>
2022-04-06 22:22:59 +0900

    Create Convertor.py

---
+++ Convertor.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+
+xlsx_file_path = "/Users/hwanju/Documents/성당/성소후원회 명단.xlsx"
+dir = os.path.split(xlsx_file_path)
+
+print(dir)
+
+
+# xlsx = pd.read_excel(xlsx_file_path)
+
+# xlsx.to_csv("Macintosh HD\user\hwanju\문서\성당\성소후원회명단.csv")
+
+
